e59ce07759c1b4452b0ab0f16eb30f6e259aff10
Yurii Zinko <yurii.zinko@vertalab.com>
2019-01-18 14:03:47 +0200

    Add O after X

--- src/index.js
+++ src/index.js
@@ -2,36 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class Square extends React.Component {
-  constructor (props) {
-    super(props)
-    this.props = {
-      value: null
-    }
-  }
-  render () {
-    return (
-      <button
-        className='square'
-        onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    )
-  }
+function Square (props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick (i) {
     const squares = this.state.squares.slice()
-    squares[i] = 'X'
-    this.setState({ squares: squares })
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   renderSquare (i) {
@@ -44,7 +38,7 @@ class Board extends React.Component {
   }
 
   render () {
-    const status = 'Next player: X'
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
 
     return (
       <div>
